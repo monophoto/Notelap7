0f02173fa9f9b42562f344455322b63a216ea171
monophoto <jetsadawut1013@gmail.com>
2022-12-13 16:23:40 +0700

    lap7_2  complete

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,10 +4,28 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
+	int i = 0, P[5000], k, result = 0, j = 0;
 	while(i < 5000){
 		P[i] = 2*i+1;
 		i++;
+	}
+
+	cout << "Please input k: ";
+	cin >> k;
+	if (k > 0){
+		while(j < 5000){
+			if(P[j]%k == 0){
+				result = result - P[j];
+				j += 1;
+			}else{
+				result = result + P[j];
+				j += 1;
+			}
+
+		}
+		cout << "Result = " << result;
+	}else{
+		cout << "Invalid input!!!";
 	}
 	
 	return 0;
