68d4165d5dc949edf37051180fcf019d8e0f737a
monophoto <jetsadawut1013@gmail.com>
2022-12-13 17:29:14 +0700

    lab7_4 complete

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -9,7 +9,18 @@ int main(){
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    //Write your code here.
+    int i = 0;
+	double GPA;
+	cout << "Please input GPA: ";
+	cin >> GPA;
+	while(i < 10){
+		if(gpa[i] >= GPA){
+			cout << id[i]<< " " << name[i]<< " " << gpa[i] << endl;
+			i++;
+		}else{
+			i++;
+		}
+	}
 		
 	return 0;
 }
